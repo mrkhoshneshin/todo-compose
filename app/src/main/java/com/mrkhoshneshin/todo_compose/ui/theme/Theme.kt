da7f05381feed05e3f33e6bda0f27d29e8a77898
MohammadJavad <mrkhoshneshin1@gmail.com>
2023-04-28 04:44:42 -0700

    mrkhoshneshin: add fonts to Theme.kt and add colors

--- app/src/main/java/com/mrkhoshneshin/todo_compose/ui/theme/Theme.kt
+++ app/src/main/java/com/mrkhoshneshin/todo_compose/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Purple,
+    primaryVariant = Purple_light,
+    secondary = Purple_light
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Purple,
+    primaryVariant = Purple_light,
+    secondary = Purple_light
 
     /* Other default colors to override
     background = Color.White,
